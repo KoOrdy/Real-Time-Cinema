313516c27395e324ef2d27683dae327d8f35b5e5
kordy <youssef591339@gmail.com>
2024-11-27 20:49:48 +0200

    config

--- Back_end/app/config/db.config.js
+++ Back_end/app/config/db.config.js
@@ -1,10 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
-<<<<<<< HEAD
-=======
     // PASSWORD: "123",
->>>>>>> 876b0ed874f868d261d694681a85f3a2d338ec9f
     PASSWORD: "123456",
     DB: "cinema",
     dialect: "postgres",
