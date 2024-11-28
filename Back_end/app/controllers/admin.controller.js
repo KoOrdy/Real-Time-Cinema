e94ae8add6d73bdb447ca34b5c7551b0bb0f87db
kordy <youssef591339@gmail.com>
2024-11-28 13:53:04 +0200

    admin edited

--- Back_end/app/controllers/admin.controller.js
+++ Back_end/app/controllers/admin.controller.js
@@ -100,3 +100,30 @@ exports.viewMovies = (req, res) => {
 
 }
 
+exports.viewAvailableMovies = async (req, res) => {
+    if (req.user.role !== "customer") {
+      return res.status(403).json({ message: "You are nor authorized to view movies." });
+    }
+    try {
+      const cinemaID = req.params.cinemaId;
+      const movies = await Movies.findAll({
+        include: [
+          {
+            model: Cinemas,
+            as: "cinemas",
+            where: { id: cinemaID },
+            required: true,
+            attributes: [],
+          },
+        ],
+      });
+      if (!movies || !movies.length) {
+        return res.status(404).json({ message: "No movies found for this cinema." });
+      }
+      return res.status(200).json({ message: "Movies fetched successfully", data: movies });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Error fetching movies:", data: error.message });
+    }
+  };
+  
