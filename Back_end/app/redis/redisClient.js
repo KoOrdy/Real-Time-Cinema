c54651647761381e668236332cdda4039890a777
kordy <youssef591339@gmail.com>
2024-12-06 02:39:40 +0200

    redis

---
+++ Back_end/app/redis/redisClient.js
@@ -0,0 +1,13 @@
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: '127.0.0.1', 
+  port: 6379,        
+});
+
+
+redisClient.on('error', (err) => {
+  console.error('Redis error: ', err);
+});
+
+module.exports = redisClient;
